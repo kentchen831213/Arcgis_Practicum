14dad5199b28bcc02bd3441f9439848816e8c184
gotoscot <gotoscot@gmail.com>
2022-03-15 22:09:26 -0500

    remap weed speed column

--- clean_dataset/clean.py
+++ clean_dataset/clean.py
@@ -1,6 +1,9 @@
+
 import csv
+import numpy as np
+import pandas as pd
 
-
+#%%
 def deal_with_initial(instring):
     res = instring[:2]
     return "".join(res.upper())
@@ -43,4 +46,17 @@ for idx, row in enumerate(csvreader):
     currow.append(row[20])
     currow.append(deal_with_windspeed(row[21]))
     writer.writerow(currow)
-
+#%%
+df = pd.read_csv('./clean_dataset/DragPts_eDNA28May21_OriginalExport.csv', skiprows=1)
+print(df)
+# change Wind speed column to shorten description.
+wind_map = {'Smoke_rises_vertically_calm': 'Calm',
+            'Smoke_drifts_slightly_light_air': 'Light Air',
+            'Leaves_rustle_light_breeze': 'Light Breeze',
+            'Leaves_constant_motion_gentle_breeze': 'Gentle Breeze',
+            'Raises_dust_sm_branches_mod_breeze': 'Moderate Breeze',
+            'Sm_trees_sway_fresh_breeze': 'Fresh Breeze',
+            'House_dropping _on_ruby_slippers': 'Strong Breeze'
+            }
+df_clean = pd.DataFrame()
+df_clean['WindSpeed'] = df['WindSpeed'].replace(wind_map)
