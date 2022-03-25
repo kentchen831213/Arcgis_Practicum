1f1dfdeb26044b5e8ca3cb3aaec23fa840218fed
Kent Chen <kent831213@gmail.com>
2022-03-25 00:52:19 -0500

    final_version

--- clean_dataset/clean.py
+++ clean_dataset/clean.py
@@ -3,59 +3,46 @@ import csv
 import numpy as np
 import pandas as pd
 
-#%%
-def deal_with_initial(instring):
-    res = instring[:2]
-    return "".join(res.upper())
 
-def deal_with_date(datestring):
-    res = datestring.split(" ")
-    return res[0]
-
-def deal_with_windspeed(speedstring):
-    res = speedstring.split("_")
-    if res[-1] == "calm" or len(res)==1:
-        return res[-1].title()
-    else:
-        return res[-2].title()+" "+res[-1].title()
-
-# open the original data csv file
-file = open('DragPts_eDNA28May21_OriginalExport.csv')
-csvreader = csv.reader(file)
-
-# create an output file
-writer = csv.writer(open('output.csv','w',newline=''))
-head = ["ObjectID",	"GlobalID",	"Initials",	"CollDate",	"FieldCrew", "SiteName", "County", "Comment_OpenText", "WindSpeed",	"CloudCover",
-        "HabitatType",	"eDNA_Smpl", "RandomNum_ID", "Number of ticks found",	"SampleID",	"eDNA_SmplID",	"Coord_X",	"Coord_Y",	"RecCheck"]
-writer.writerow(head)
-
-# 1	6622b5e3-7f68-4c8e-9f87-a0cee224fd26	JT	6/1/2021	Lee, Tinoco, Kopsco, UruchimaLamczykProperty	Jefferson
-# Its on & on drizzling 	Calm	OVC	Gr			0			-88.9756348	38.3032935
-rows = []
-i = 0
-for idx, row in enumerate(csvreader):
-    if idx == 0 or idx == 1: continue
-    currow = []
-    currow.append(row[0])
-    currow.append(row[1])
-    currow.append(deal_with_initial(row[3]))
-    currow.append(deal_with_date(row[2]))
-    currow.append(row[7])
-    currow.append(" ")
-    currow.append(row[11])
-    currow.append(row[20])
-    currow.append(deal_with_windspeed(row[21]))
-    writer.writerow(currow)
-#%%
-df = pd.read_csv('./clean_dataset/DragPts_eDNA28May21_OriginalExport.csv', skiprows=1)
+df = pd.read_csv('./DragPts_eDNA28May21_OriginalExport.csv', skiprows=1)
 print(df)
 # adjust Wind speed column.
 """
-dictionary for the mapping of description: 
+dictionary for the mapping of description:
 When there is a new values for survey 123
 1. add comma to the last row of the dictionary ex: 'House_dropping _on_ruby_slippers': 'Strong Breeze', <= comma
 2. add 'original':'new description' to a new row
 """
+
+# claim a structure to save cleaned data
+df_clean = pd.DataFrame()
+
+# rename and copy column
+df_clean[['ObjectID', 'GlobalID']] = df[['ObjectID', 'GlobalID']]
+
+# adjust Creator column.
+df['Creator_substring'] = df['Creator'].str.upper()
+# do the remapping
+df_clean['Initials'] = df['Creator_substring'].str.slice(0, 2)
+
+# adjust CreationDate column.
+new = df["CreationDate"].str.split(" ", n = 1, expand = True)
+# do the remapping
+df_clean['CollDate'] = new[0]
+
+# adjust FieldCrew column
+df_clean['FieldCrew'] = df['FieldCrew'].fillna('') + ','+df['Other - FieldCrew'].fillna('')
+
+#%%
+# combine multiple sites columns into one
+df_clean['SiteName'] = df['SitesChampaignCo'].fillna('') + df['SiteFordCo'].fillna('') + df['SitesHamiltonCo'].fillna('')\
++ df['SitesJacksonCo'].fillna('') + df['SitesJeffersonCo'].fillna('') + df['SitesPerryCo'].fillna('')\
++ df['SitesPopeCo'].fillna('') + df['SitesWilliamsonCo'].fillna('') + df['Other - SiteName'].fillna('')
+
+# rename and copy column
+df_clean[['County', 'Comment_OpenText']] = df[['County', 'Comment_OpenText']]
+
+# do the remapping
 wind_map = {'Smoke_rises_vertically_calm': 'Calm',
             'Smoke_drifts_slightly_light_air': 'Light Air',
             'Leaves_rustle_light_breeze': 'Light Breeze',
@@ -64,9 +51,7 @@ wind_map = {'Smoke_rises_vertically_calm': 'Calm',
             'Sm_trees_sway_fresh_breeze': 'Fresh Breeze',
             'House_dropping _on_ruby_slippers': 'Strong Breeze'
             }
-# claim a structure to save cleaned data
-df_clean = pd.DataFrame()
-# do the remapping
+
 df_clean['WindSpeed'] = df['WindSpeed'].replace(wind_map)
 
 #%%
@@ -101,11 +86,5 @@ df_clean['NumTicks'].fillna(0, inplace=True)
 df_clean = df_clean.astype({'NumTicks': 'int32'})
 
 #%%
-# combine multiple sites columns into one
-df_clean['SiteName'] = df['SitesChampaignCo'].fillna('') + df['SiteFordCo'].fillna('') + df['SitesHamiltonCo'].fillna('')\
-+ df['SitesJacksonCo'].fillna('') + df['SitesJeffersonCo'].fillna('') + df['SitesPerryCo'].fillna('')\
-+ df['SitesPopeCo'].fillna('') + df['SitesWilliamsonCo'].fillna('') + df['Other - SiteName'].fillna('')
-
-#%%
 # write to cav file
-df_clean.to_csv('./clean_dataset/cleaned.csv', index=False)
+df_clean.to_csv('./cleaned.csv', index=False)
